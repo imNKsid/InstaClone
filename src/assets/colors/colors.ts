307c5244a82468383b0e318dde65b2354d5879fa
Nitesh Kumar <imnksid@gmail.com>
2024-07-05 14:06:40 +0530

    Changed the Colors file

--- src/assets/colors/colors.ts
+++ src/assets/colors/colors.ts
@@ -1,15 +1,7 @@
 const COLORS = {
-  white: '#FFFFFF',
-  black: '#000000',
-  gray: '#8891A5',
-  primaryBlue: '#2A4BA0',
-  darkBlue: '#153075',
-  darkGray: '#8891A5',
-  lightGray: '#F8F9FB',
-  primaryGray: '#EBEDED',
-  textColor: '#1E222B',
-  lightBlue: '#616A7D',
-  yellow: '#F9B023',
+  white: "#FFFFFF",
+  black: "#000000",
+  purple: "#AE22E0",
 };
 
 export default COLORS;
