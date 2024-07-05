c0e12098ad6ea6b47eaf460f7cd02883e89eb766
Nitesh Kumar <imnksid@gmail.com>
2024-07-05 14:07:03 +0530

    Imported ProfilePicture component in Home screen

--- src/screens/home-screen/index.tsx
+++ src/screens/home-screen/index.tsx
@@ -1,10 +1,20 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { ProfilePicture } from "../../components";
 
 const Home = () => {
   return (
     <View>
       <Text>Home</Text>
+      <ProfilePicture
+        uri={"https://randomuser.me/api/portraits/women/58.jpg"}
+      />
+      <ProfilePicture
+        uri={"https://randomuser.me/api/portraits/med/men/25.jpg"}
+      />
+      <ProfilePicture
+        uri={"https://randomuser.me/api/portraits/med/women/89.jpg"}
+      />
     </View>
   );
 };
