40331dfdc5541a11232cd524a79b166de924a226
Nitesh Kumar <imnksid@gmail.com>
2024-07-06 16:39:18 +0530

    Added Logo into the Home screen

--- src/screens/home-screen/index.tsx
+++ src/screens/home-screen/index.tsx
@@ -1,21 +1,13 @@
-import { View, Text } from "react-native";
+import { Image, SafeAreaView } from "react-native";
 import React from "react";
-import { ProfilePicture } from "../../components";
+import { IMAGES } from "../../assets";
+import { Styles } from "./style";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-      <ProfilePicture
-        uri={"https://randomuser.me/api/portraits/women/58.jpg"}
-      />
-      <ProfilePicture
-        uri={"https://randomuser.me/api/portraits/med/men/25.jpg"}
-      />
-      <ProfilePicture
-        uri={"https://randomuser.me/api/portraits/med/women/89.jpg"}
-      />
-    </View>
+    <SafeAreaView>
+      <Image source={IMAGES.logo} style={Styles.mainLogo} />
+    </SafeAreaView>
   );
 };
 
