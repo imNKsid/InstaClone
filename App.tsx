0a6a2ebb0669b80ce6bb26873d56d51c437639f8
Nitesh Kumar <imnksid@gmail.com>
2024-07-13 21:48:27 +0530

    Redux store put into App file

--- App.tsx
+++ App.tsx
@@ -2,12 +2,16 @@ import { View, Text } from "react-native";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./src/navigation/app-navigator";
+import { Provider } from "react-redux";
+import { store } from "./src/redux/store/store";
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
