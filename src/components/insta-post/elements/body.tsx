07ef936fd56ae6712f8a479183c36b3393e51fa9
Nitesh Kumar <imnksid@gmail.com>
2024-07-21 18:43:28 +0530

    Added props & a image in Post's body

--- src/components/insta-post/elements/body.tsx
+++ src/components/insta-post/elements/body.tsx
@@ -1,14 +1,24 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-const Body = () => {
+interface BodyProps {
+  imageUri: string;
+}
+
+const Body = (props: BodyProps) => {
+  const { imageUri } = props;
   return (
     <View>
-      <Text>Body</Text>
+      <Image source={{ uri: imageUri }} style={styles.image} />
     </View>
   );
 };
 
 export default Body;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 600,
+  },
+});
