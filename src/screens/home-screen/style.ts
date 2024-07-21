a205fd8dbc267d5c9305d8ee31e11b8779d562bd
Nitesh Kumar <imnksid@gmail.com>
2024-07-21 18:46:12 +0530

    Created Home screen style

--- src/screens/home-screen/style.ts
+++ src/screens/home-screen/style.ts
@@ -3,6 +3,10 @@ import { Dimensions, StyleSheet } from "react-native";
 const { width, height } = Dimensions.get("window");
 
 export const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginBottom: 80,
+  },
   mainLogo: {
     height: height * 0.06,
     width: width * 0.5,
