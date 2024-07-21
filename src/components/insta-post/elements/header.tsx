f71ce162825b5d0a97d3cf2975cd9f202f0d0384
Nitesh Kumar <imnksid@gmail.com>
2024-07-21 18:44:10 +0530

    Added ProfilePicture & name in Post's Header

--- src/components/insta-post/elements/header.tsx
+++ src/components/insta-post/elements/header.tsx
@@ -1,15 +1,41 @@
 import { StyleSheet, Text, View } from "react-native";
-import React, { useEffect } from "react";
-import axios from "axios";
+import React from "react";
+import ProfilePicture from "../../profile-picture";
+import { scaler } from "../../../utils";
+import { COLORS } from "../../../assets";
 
-const Header = () => {
+interface HeaderProps {
+  imageUri: string;
+  name: string;
+}
+
+const Header = (props: HeaderProps) => {
+  const { imageUri, name } = props;
   return (
-    <View>
-      <Text>Header</Text>
+    <View style={styles.container}>
+      <View style={styles.nameNpic}>
+        <ProfilePicture uri={imageUri} size={40} />
+        <Text style={styles.username}>{name}</Text>
+      </View>
     </View>
   );
 };
 
 export default Header;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  nameNpic: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  username: {
+    fontSize: scaler(13),
+    fontWeight: "500",
+    color: COLORS.lightBlack,
+  },
+});
