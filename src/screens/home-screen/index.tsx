8896ffc892756e0d6c00a3c97737a55537f90fc2
Nitesh Kumar <imnksid@gmail.com>
2024-07-21 18:45:26 +0530

    Added redux variables in Home screen

--- src/screens/home-screen/index.tsx
+++ src/screens/home-screen/index.tsx
@@ -1,15 +1,30 @@
-import { Image, SafeAreaView } from "react-native";
-import React from "react";
+import { Image, SafeAreaView, ScrollView } from "react-native";
+import React, { useEffect } from "react";
 import { Post, Stories } from "../../components";
 import { IMAGES } from "../../assets";
 import { Styles } from "./style";
+import UsersThunk from "../../redux/ducks/users/users-thunk";
+import PostsThunk from "../../redux/ducks/posts/posts-thunk";
+import { dispatch } from "../../redux/store/store";
+import PostsSelector from "../../redux/ducks/posts/posts-selector";
+import UserSelector from "../../redux/ducks/users/users-selector";
 
 const Home = () => {
+  const usersData = UserSelector.usersData();
+  const postsData = PostsSelector.postsData();
+
+  useEffect(() => {
+    dispatch<any>(UsersThunk.getAllUsers());
+    dispatch<any>(PostsThunk.getAllPosts());
+  }, []);
+
   return (
-    <SafeAreaView>
-      <Image source={IMAGES.logo} style={Styles.mainLogo} />
-      <Stories />
-      <Post />
+    <SafeAreaView style={Styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Image source={IMAGES.logo} style={Styles.mainLogo} />
+        <Stories usersData={usersData} />
+        <Post postData={postsData} />
+      </ScrollView>
     </SafeAreaView>
   );
 };
