2704113f76bc4223ea1c251e553497319e28b1ab
Nitesh Kumar <imnksid@gmail.com>
2024-07-21 19:09:31 +0530

    Passing props in Header, Body & Footer for Post component

--- src/components/insta-post/index.tsx
+++ src/components/insta-post/index.tsx
@@ -2,12 +2,15 @@ import { View, Text } from "react-native";
 import React from "react";
 import { Body, Footer, Header } from "./elements";
 
-const Post = () => {
+const Post = ({ postData }: any) => {
   return (
     <View>
-      <Header />
-      <Body />
-      <Footer />
+      <Header
+        imageUri={postData[0]?.owner?.picture}
+        name={postData[0]?.owner?.firstName}
+      />
+      <Body imageUri={postData[0]?.image} />
+      <Footer postData={postData[0]} />
     </View>
   );
 };
