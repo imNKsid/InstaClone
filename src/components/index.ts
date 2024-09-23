8343bab9a530849f4d48a1c3eb20628857f6f717
Nitesh Kumar <imnksid@gmail.com>
2024-09-23 18:20:36 +0530

    Exported Feed component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import ProfilePicture from "./profile-picture";
 import Story from "./story";
 import Stories from "./stories";
 import Post from "./insta-post";
+import Feed from "./feed";
 
-export { ProfilePicture, Story, Stories, Post };
+export { ProfilePicture, Story, Stories, Post, Feed };
