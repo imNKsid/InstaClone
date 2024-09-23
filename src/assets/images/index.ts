ca6673de410505a0debce576ee1c40e4aa0dd6cb
Nitesh Kumar <imnksid@gmail.com>
2024-09-23 17:54:10 +0530

    Added menu, heart, comment, send & save images

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,10 @@
 export const IMAGES = {
   logo: require("./logo.png"),
+  menu: require("./menu.png"),
+  heartEmpty: require("./heart-empty.png"),
+  heartFilled: require("./heart-filled.png"),
+  comment: require("./comment.png"),
+  send: require("./send.png"),
+  saveEmpty: require("./save-empty.png"),
+  saveFilled: require("./save-filled.png"),
 };
