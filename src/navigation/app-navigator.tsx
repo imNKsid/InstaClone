4be6388c60b463b82996fc4d1bb43bef1b994155
Nitesh Kumar <imnksid@gmail.com>
2024-10-01 17:30:26 +0530

    Added all the bottom tabs

--- src/navigation/app-navigator.tsx
+++ src/navigation/app-navigator.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import { Image, Platform, StyleSheet, Text, View } from "react-native";
-
+import { Image, StyleSheet, Text, View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import DeviceInfo from "react-native-device-info";
-
-// import {CategoryScreen, FavouriteScreen, MoreScreen} from '../screens';
-import { COLORS } from "../assets";
+import { COLORS, IMAGES } from "../assets";
 import HomeStack from "./home-stack";
+import { AddPost, Favourite, Search, User } from "../screens";
 
 const Tab = createBottomTabNavigator();
 
@@ -22,73 +19,42 @@ const AppNavigator = () => {
       <Tab.Screen
         name="Home"
         component={HomeStack}
-        // options={tabOptions(
-        //   IMAGES.home_selected,
-        //   IMAGES.home_unselected,
-        //   'Home',
-        //   false,
-        // )}
+        options={tabOptions(IMAGES.homeSelected, IMAGES.homeUnselected)}
       />
-      {/* <Tab.Screen
-        name="Category"
-        component={CategoryScreen}
-        // options={tabOptions(
-        //   IMAGES.category_selected,
-        //   IMAGES.category_unselected,
-        //   'Categories',
-        //   true,
-        // )}
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={tabOptions(IMAGES.searchSelected, IMAGES.searchUnselected)}
+      />
+      <Tab.Screen
+        name="Add"
+        component={AddPost}
+        options={tabOptions(IMAGES.addSelected, IMAGES.addUnselected)}
       />
       <Tab.Screen
         name="Favourite"
-        component={FavouriteScreen}
-        // options={tabOptions(
-        //   IMAGES.fav_selected,
-        //   IMAGES.fav_unselected,
-        //   'Favourite',
-        //   true,
-        // )}
+        component={Favourite}
+        options={tabOptions(IMAGES.heartSelected, IMAGES.heartUnselected)}
       />
       <Tab.Screen
-        name="More"
-        component={MoreScreen}
-        // options={tabOptions(
-        //   IMAGES.more_selected,
-        //   IMAGES.more_unselected,
-        //   'More',
-        //   true,
-        // )}
-      /> */}
+        name="User"
+        component={User}
+        options={tabOptions(IMAGES.userSelected, IMAGES.userUnselected)}
+      />
     </Tab.Navigator>
   );
 };
 
 export default AppNavigator;
 
-const tabOptions = (
-  iconActive: any,
-  iconInactive: any,
-  labelText: string,
-  showBlackCircle: boolean
-) => ({
-  tabBarLabel: ({ focused }: { focused: boolean }) => (
-    <Text style={styles.label}>{focused ? "" : labelText}</Text>
-  ),
-  tabBarLabelStyle: styles.label,
+const tabOptions = (iconActive: any, iconInactive: any) => ({
+  tabBarLabel: () => <View />,
   tabBarIcon: ({ focused }: { focused: boolean }) => (
     <>
       {focused ? (
-        <View style={styles.selectedTab}>
-          {showBlackCircle ? (
-            <View style={styles.blackCircle}>
-              <Image source={iconActive} style={styles.categorySelectedIcon} />
-            </View>
-          ) : (
-            <Image source={iconActive} style={styles.selectedIcon} />
-          )}
-        </View>
+        <Image source={iconActive} style={styles.icon} />
       ) : (
-        <Image source={iconInactive} style={styles.unSelectedIcon} />
+        <Image source={iconInactive} style={styles.icon} />
       )}
     </>
   ),
@@ -96,44 +62,13 @@ const tabOptions = (
 
 const styles = StyleSheet.create({
   tabBar: {
-    // height: DeviceInfo.hasNotch() && Platform.OS === "android" ? 103 : 65,
+    // height: 60,
     position: "absolute",
     bottom: 5,
-    borderRadius: 30,
     backgroundColor: COLORS.white,
   },
-  label: {
-    color: COLORS.lightBlack,
-    paddingBottom: 10,
-  },
-  selectedTab: {
-    justifyContent: "center",
-    alignItems: "center",
-    height: 75,
-    width: 75,
-    borderRadius: 75 / 2,
-    marginBottom: 20,
-    backgroundColor: "rgba(256, 256, 256, 0.1)",
-  },
-  selectedIcon: {
-    height: 55,
-    width: 55,
-  },
-  unSelectedIcon: {
-    marginTop: 10,
+  icon: {
     height: 25,
     width: 25,
-  },
-  categorySelectedIcon: {
-    height: 25,
-    width: 25,
-  },
-  blackCircle: {
-    justifyContent: "center",
-    alignItems: "center",
-    height: 55,
-    width: 55,
-    borderRadius: 55 / 2,
-    backgroundColor: COLORS.black,
   },
 });
