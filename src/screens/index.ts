a603a0cc8871d7e6ed93d660252be631b35121ae
Nitesh Kumar <imnksid@gmail.com>
2024-10-01 17:30:45 +0530

    Added all the bottom screens

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,3 +1,7 @@
 import Home from "./home-screen";
+import Search from "./search-screen";
+import AddPost from "./add-screen";
+import Favourite from "./heart-screen";
+import User from "./user-screen";
 
-export { Home };
+export { Home, Search, AddPost, Favourite, User };
