a176c98bd1f9a5c9ef3666ad009d6c3235cebada
Nitesh Kumar <imnksid@gmail.com>
2024-10-02 16:19:07 +0530

    Exporting StoryScreen

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -3,5 +3,6 @@ import Search from "./search-screen";
 import AddPost from "./add-screen";
 import Favourite from "./heart-screen";
 import User from "./user-screen";
+import StoryScreen from "./story-screen";
 
-export { Home, Search, AddPost, Favourite, User };
+export { Home, Search, AddPost, Favourite, User, StoryScreen };
