42ec2903c83787802a857aac7c9e8aeb37fc142e
Nitesh Kumar <imnksid@gmail.com>
2024-10-02 16:18:42 +0530

    Added id & userId in UserProps & StoryProps resp.

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -2,9 +2,11 @@ export interface UserProps {
   firstName: string;
   lastName: string;
   picture: string;
+  id: string;
 }
 
 export interface StoryProps {
   imageUri: string;
   name: string;
+  userId: string;
 }
