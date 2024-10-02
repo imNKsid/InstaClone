6cfc4683025d37156940c8afe55c1095654c7c67
Nitesh Kumar <imnksid@gmail.com>
2024-10-02 16:19:46 +0530

    Created API to fetch user stories

--- src/redux/ducks/posts/posts-thunk.ts
+++ src/redux/ducks/posts/posts-thunk.ts
@@ -20,8 +20,28 @@ const getAllPosts = createAsyncThunk(
   }
 );
 
+const getPostsByUserId = createAsyncThunk(
+  "getPostsByUserId",
+  async (id: string, { rejectWithValue, dispatch }) => {
+    const res = (await PostsService.getRequest(`user/${id}/post`)) as any;
+
+    // console.log("getPostsByUserId res =>", JSON.stringify(res));
+
+    if (
+      res?.data &&
+      res?.status === 200 &&
+      res.data?.data &&
+      res.data.data.length > 0
+    ) {
+      return res.data.data;
+    }
+    return [];
+  }
+);
+
 const PostsThunk = {
   getAllPosts,
+  getPostsByUserId,
 };
 
 export default PostsThunk;
