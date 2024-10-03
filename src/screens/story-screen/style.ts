fdbc4c97e5b91a1c46a21e2a5f92ef11dc2747c4
Nitesh Kumar <imnksid@gmail.com>
2024-10-03 08:21:28 +0530

    Adding styles for storyImage & loader

--- src/screens/story-screen/style.ts
+++ src/screens/story-screen/style.ts
@@ -1,8 +1,17 @@
 import { StyleSheet } from "react-native";
+import { WindowDimensions } from "../../utils";
 
 export const Styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginBottom: 80,
+  },
+  storyImage: {
+    width: WindowDimensions.WIDTH,
+    height: WindowDimensions.HEIGHT * 0.95,
+    resizeMode: "cover",
+  },
+  loader: {
+    flex: 1,
+    marginTop: WindowDimensions.HEIGHT / 2.5,
   },
 });
