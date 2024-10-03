c3d7f77db421f5a19870af53ae4481e4ffcaf12b
Nitesh Kumar <imnksid@gmail.com>
2024-10-03 08:20:08 +0530

    Navigating the user to StoryScreen on story click & calling API

--- src/components/story.tsx
+++ src/components/story.tsx
@@ -1,15 +1,31 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import ProfilePicture from "./profile-picture";
 import { StoryProps } from "../utils/interfaces";
+import { StackActions, useNavigation } from "@react-navigation/native";
+import { dispatch } from "../redux/store/store";
+import PostsThunk from "../redux/ducks/posts/posts-thunk";
 
 const Story = (props: StoryProps) => {
-  const { imageUri, name } = props;
+  const { imageUri, name, userId } = props;
+  const navigation = useNavigation();
+
   return (
-    <View style={styles.storyStyle}>
+    <TouchableOpacity
+      style={styles.storyStyle}
+      onPress={() => {
+        navigation.dispatch(
+          StackActions.push("WithoutBottom", {
+            screen: "StoryScreen",
+            params: { userId },
+          })
+        );
+        dispatch<any>(PostsThunk.getPostsByUserId(userId));
+      }}
+    >
       <ProfilePicture uri={imageUri} />
       <Text style={styles.userName}>{name}</Text>
-    </View>
+    </TouchableOpacity>
   );
 };
 
