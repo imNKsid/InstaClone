69e4dd0412178d60cb4fbf9fa8dabd3b0213b447
Nitesh Kumar <imnksid@gmail.com>
2024-10-05 17:06:00 +0530

    Exporting formatDate & timeDifferenceFromNow functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,12 @@
 import scaler from "./scaler";
 import { WindowDimensions, isAndroid, isIOS } from "./globalConstants";
+import { formatDate, timeDifferenceFromNow } from "./general-utils";
 
-export { scaler, WindowDimensions, isAndroid, isIOS };
+export {
+  scaler,
+  WindowDimensions,
+  isAndroid,
+  isIOS,
+  formatDate,
+  timeDifferenceFromNow,
+};
