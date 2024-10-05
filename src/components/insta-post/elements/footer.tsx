68926ca386863b2f959032eb62587d45f87fd362
Nitesh Kumar <imnksid@gmail.com>
2024-10-05 17:04:35 +0530

    Remove formatDate function

--- src/components/insta-post/elements/footer.tsx
+++ src/components/insta-post/elements/footer.tsx
@@ -51,7 +51,6 @@ const Footer = ({ postData }: any) => {
         <Text style={styles.likeStyle}>{postData?.owner?.firstName}</Text>
         <Text>{text}</Text>
       </View>
-      <Text>{formatDate(publishDate)}</Text>
     </View>
   );
 };
@@ -85,32 +84,3 @@ const styles = StyleSheet.create({
   },
   caption: { flexDirection: "row" },
 });
-
-function formatDate(dateString: string): string {
-  const date = new Date(dateString);
-  const now = new Date();
-
-  const diffInSeconds = Math.floor((now.getTime() - date.getTime()) / 1000);
-  const diffInMinutes = Math.floor(diffInSeconds / 60);
-  const diffInHours = Math.floor(diffInMinutes / 60);
-  const diffInDays = Math.floor(diffInHours / 24);
-
-  if (diffInSeconds < 60) {
-    return `${diffInSeconds} seconds ago`;
-  } else if (diffInMinutes < 60) {
-    return `${diffInMinutes} minutes ago`;
-  } else if (diffInHours < 24) {
-    return `${diffInHours} hours ago`;
-  } else if (diffInDays < 7) {
-    return `${diffInDays} days ago`;
-  } else {
-    const options: Intl.DateTimeFormatOptions = {
-      day: "numeric",
-      month: "short",
-    };
-    if (date.getFullYear() !== now.getFullYear()) {
-      options.year = "numeric";
-    }
-    return date.toLocaleDateString("en-US", options);
-  }
-}
