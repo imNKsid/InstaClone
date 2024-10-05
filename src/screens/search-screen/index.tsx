aa6969987422a803ef57b47f6206d26d366cce8e
Nitesh Kumar <imnksid@gmail.com>
2024-10-05 20:17:29 +0530

    Added Giphy data into search screen, just to look good

--- src/screens/search-screen/index.tsx
+++ src/screens/search-screen/index.tsx
@@ -1,11 +1,50 @@
-import { SafeAreaView, ScrollView } from "react-native";
-import React from "react";
+import { Image, SafeAreaView, ScrollView, TextInput, View } from "react-native";
+import React, { useState } from "react";
 import { Styles } from "./style";
+import {
+  GiphyContent,
+  GiphyGridView,
+  GiphyMedia,
+  GiphySDK,
+} from "@giphy/react-native-sdk";
+import { IMAGES } from "../../assets";
+
+const key = "HG3g4GJ0BLvcXTFzmRM4Z5I8D9H35vKD";
+
+GiphySDK.configure({ apiKey: key });
 
 const Search = () => {
+  const [searchText, setSearchText] = useState("");
+
   return (
     <SafeAreaView style={Styles.container}>
-      <ScrollView showsVerticalScrollIndicator={false}></ScrollView>
+      <View style={Styles.searchBox}>
+        <Image source={IMAGES.search} style={Styles.searchImg} />
+        <TextInput
+          placeholder="Search"
+          value={searchText}
+          onChangeText={setSearchText}
+          style={Styles.searchInput}
+          placeholderTextColor={"gray"}
+        />
+      </View>
+      {searchText.length > 3 ? (
+        <>
+          <GiphyGridView
+            content={GiphyContent.search({
+              searchQuery: searchText,
+            })}
+            cellPadding={4}
+            style={{ flex: 1 }}
+          />
+        </>
+      ) : (
+        <GiphyGridView
+          content={GiphyContent.trendingGifs()}
+          cellPadding={4}
+          style={{ flex: 1 }}
+        />
+      )}
     </SafeAreaView>
   );
 };
